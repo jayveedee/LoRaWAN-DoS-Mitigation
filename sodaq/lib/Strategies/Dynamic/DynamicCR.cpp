56d455a9bce3b26b7797ecc581740e3130044e98
Jákup Viljam Dam <jayveedees@gmail.com>
2025-05-15 15:02:16 +0200

    add todo

--- sodaq/lib/Strategies/Dynamic/DynamicCR.cpp
+++ sodaq/lib/Strategies/Dynamic/DynamicCR.cpp
@@ -1,5 +1,6 @@
 #include "DynamicCR.h"
 
+// TODO if continuing with this, refactor usage of mac commands and use radio instead
 DynamicCR::DynamicCR(Stream *console, Sodaq_RN2483 *loRaBee, void (*setRgbColorCallback)(uint8_t, uint8_t, uint8_t))
     : BaseDynamic(console, loRaBee, setRgbColorCallback)
 {
