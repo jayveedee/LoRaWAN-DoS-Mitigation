7b9fbbf73a2b53742b7467a266501486b0649c02
Jákup Viljam Dam <jayveedees@gmail.com>
2025-05-17 12:23:00 +0200

    remove LBT, start from scratch

--- sodaq/lib/Strategies/ListenBeforeTalk/LBT.h
+++ sodaq/lib/Strategies/ListenBeforeTalk/LBT.h
@@ -1,50 +1,8 @@
 #ifndef LBT_H
 #define LBT_H
 
-#include <Arduino.h>
 #include "../BaseStrategy.h"
 
-// Default configuration parameters
-#define DEFAULT_LORA_CHANNEL 0
-#define DEFAULT_DUMMY_PAYLOAD_SIZE 4
-#define DEFAULT_LISTEN_TIMEOUT 200
-#define DEFAULT_RSSI_THRESHOLD -90
-#define DEFAULT_MAX_RETRY_COUNT 5
 
-// Jamming detection statistics structure
-struct JammingStats
-{
-  unsigned long totalTransmissions;
-  unsigned long jammingDetected;
-  unsigned long lastJammingTime;
-  byte currentChannel;
-  byte retryCount;
-};
-
-class LBT : public BaseStrategy
-{
-private:
-  // Configuration parameters
-  int16_t _rssiThreshold;
-  uint16_t _listenTimeout;
-  byte _maxRetryCount;
-  byte _dummyPayloadSize;
-
-  // Jamming statistics
-  JammingStats _jammingStats;
-
-  // Internal helpers
-  void implementMitigationStrategy();
-  bool detectJamming();
-
-public:
-  LBT(Stream *console, Sodaq_RN2483 *loRaBee, void (*setRgbColorCallback)(uint8_t, uint8_t, uint8_t));
-
-  // Core functionality
-  virtual bool sendMessage(uint8_t port, uint8_t *buffer, uint8_t size, uint8_t &count) override;
-
-  // Statistics and logging
-  void logJammingEvent();
-};
 
 #endif
