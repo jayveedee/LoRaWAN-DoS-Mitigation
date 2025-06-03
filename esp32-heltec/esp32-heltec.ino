889a5c9ac237d2680c727afe65a893603602ddf8
Mohammad Uddin <s184174@student.dtu.dk>
2025-06-03 16:34:56 +0200

    Attempt to join with otaa

--- esp32-heltec/esp32-heltec.ino
+++ esp32-heltec/esp32-heltec.ino
@@ -1,49 +1,144 @@
-#include "LoRaWan_APP.h"
-#include "Arduino.h"
+/* Heltec Automation LoRaWAN communication example
+ *
+ * Function:
+ * 1. Upload node data to the server using the standard LoRaWAN protocol.
+ *  
+ * Description:
+ * 1. Communicate using LoRaWAN protocol.
+ * 
+ * HelTec AutoMation, Chengdu, China
+ * 成都惠利特自动化科技有限公司
+ * www.heltec.org
+ *
+ * */
 
-static RadioEvents_t RadioEvents;
+#include "LoRaWan_APP.h"
+
+/* OTAA para*/
+uint8_t devEui[] = { 0x70, 0xB3, 0xD5, 0x7E, 0xD0, 0x07, 0x13, 0x09 };
+uint8_t appEui[]    = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02 };
+uint8_t appKey[]    = { 0x7E, 0xAB, 0x4C, 0x95, 0xCF, 0x55, 0xAB, 0xE5,
+                 0xDF, 0x81, 0x27, 0xCF, 0x6D, 0x0A, 0xD6, 0x18 };
+
+
+/* ABP para*/
+uint8_t nwkSKey[] = { 0 };
+uint8_t appSKey[] = { 0 };
+uint32_t devAddr =  0;
+
+/*LoraWan channelsmask, default channels 0-7*/ 
+uint16_t userChannelsMask[6]={ 0x00FF,0x0000,0x0000,0x0000,0x0000,0x0000 };
+
+/*LoraWan region, select in arduino IDE tools*/
+LoRaMacRegion_t loraWanRegion = LORAMAC_REGION_EU868;
+
+/*LoraWan Class, Class A and Class C are supported*/
+DeviceClass_t  loraWanClass = CLASS_A;
+
+/*the application data transmission duty cycle.  value in [ms].*/
+uint32_t appTxDutyCycle = 15000;
+
+/*OTAA or ABP*/
+bool overTheAirActivation = true;
+
+/*ADR enable*/
+bool loraWanAdr = true;
+
+/* Indicates if the node is sending confirmed or unconfirmed messages */
+bool isTxConfirmed = true;
+
+/* Application port */
+uint8_t appPort = 2;
+/*!
+* Number of trials to transmit the frame, if the LoRaMAC layer did not
+* receive an acknowledgment. The MAC performs a datarate adaptation,
+* according to the LoRaWAN Specification V1.0.2, chapter 18.4, according
+* to the following table:
+*
+* Transmission nb | Data Rate
+* ----------------|-----------
+* 1 (first)       | DR
+* 2               | DR
+* 3               | max(DR-1,0)
+* 4               | max(DR-1,0)
+* 5               | max(DR-2,0)
+* 6               | max(DR-2,0)
+* 7               | max(DR-3,0)
+* 8               | max(DR-3,0)
+*
+* Note, that if NbTrials is set to 1 or 2, the MAC will not decrease
+* the datarate, in case the LoRaMAC layer did not receive an acknowledgment
+*/
+uint8_t confirmedNbTrials = 4;
+
+/* Prepares the payload of the frame */
+static void prepareTxFrame( uint8_t port )
+{
+  /*appData size is LORAWAN_APP_DATA_MAX_SIZE which is defined in "commissioning.h".
+  *appDataSize max value is LORAWAN_APP_DATA_MAX_SIZE.
+  *if enabled AT, don't modify LORAWAN_APP_DATA_MAX_SIZE, it may cause system hanging or failure.
+  *if disabled AT, LORAWAN_APP_DATA_MAX_SIZE can be modified, the max value is reference to lorawan region and SF.
+  *for example, if use REGION_CN470, 
+  *the max value for different DR can be found in MaxPayloadOfDatarateCN470 refer to DataratesCN470 and BandwidthsCN470 in "RegionCN470.h".
+  */
+    appDataSize = 4;
+    appData[0] = 0x00;
+    appData[1] = 0x01;
+    appData[2] = 0x02;
+    appData[3] = 0x03;
+}
+
+//if true, next uplink will add MOTE_MAC_DEVICE_TIME_REQ 
+
 
 void setup() {
   Serial.begin(115200);
-  delay(1500);
-
-  Serial.println("Initializing SX1262 LoRa...");
-
-  // Attach empty callbacks (or real ones if needed)
-  RadioEvents.TxDone = NULL;
-  RadioEvents.RxDone = NULL;
-  RadioEvents.TxTimeout = NULL;
-  RadioEvents.RxTimeout = NULL;
-  RadioEvents.RxError = NULL;
-
-  Radio.Init(&RadioEvents);  // ✅ fix: pass pointer to RadioEvents
-
-  Radio.SetChannel(868000000);  // Set frequency (Europe)
-
-  // Configure TX parameters
-  Radio.SetTxConfig(
-    MODEM_LORA,
-    14,     // TX power
-    0,      // Freq deviation (unused)
-    0,      // Bandwidth (0 = 125kHz)
-    7,      // SF7
-    1,      // Coding rate 4/5
-    8,      // Preamble length
-    false,  // Fixed length
-    true,   // CRC
-    0,      // Frequency hopping
-    0,      // Hop period
-    false,  // IQ inversion
-    3000    // Timeout
-  ); 
-
-  Serial.println("LoRa ready. Sending packets...");
+  Mcu.begin(HELTEC_BOARD,SLOW_CLK_TPYE);
 }
 
-void loop() {
-  uint8_t payload[] = "Hello from Heltec SX1262";
-  Radio.Send(payload, sizeof(payload));
-  Serial.println("Packet sent!");
-
-  delay(5000);
+void loop()
+{
+  switch( deviceState )
+  {
+    case DEVICE_STATE_INIT:
+    {
+#if(LORAWAN_DEVEUI_AUTO)
+      LoRaWAN.generateDeveuiByChipID();
+#endif
+      LoRaWAN.init(loraWanClass,loraWanRegion);
+      //both set join DR and DR when ADR off 
+      LoRaWAN.setDefaultDR(3);
+      break;
+    }
+    case DEVICE_STATE_JOIN:
+    {
+      LoRaWAN.join();
+      break;
+    }
+    case DEVICE_STATE_SEND:
+    {
+      prepareTxFrame( appPort );
+      LoRaWAN.send();
+      deviceState = DEVICE_STATE_CYCLE;
+      break;
+    }
+    case DEVICE_STATE_CYCLE:
+    {
+      // Schedule next packet transmission
+      txDutyCycleTime = appTxDutyCycle + randr( -APP_TX_DUTYCYCLE_RND, APP_TX_DUTYCYCLE_RND );
+      LoRaWAN.cycle(txDutyCycleTime);
+      deviceState = DEVICE_STATE_SLEEP;
+      break;
+    }
+    case DEVICE_STATE_SLEEP:
+    {
+      LoRaWAN.sleep(loraWanClass);
+      break;
+    }
+    default:
+    {
+      deviceState = DEVICE_STATE_INIT;
+      break;
+    }
+  }
 }
