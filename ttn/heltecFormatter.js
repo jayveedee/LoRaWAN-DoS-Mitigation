24b048cea2149a9897262f1f8d492d1c14b01e18
Jákup Viljam Dam <jvd@exirpit.com>
2025-06-09 16:23:42 +0200

    format

--- ttn/heltecFormatter.js
+++ ttn/heltecFormatter.js
@@ -4,7 +4,7 @@ function decodeUplink(input) {
   return {
     data: {
       text: String.fromCharCode(bytes[0], bytes[1], bytes[2], bytes[3]),
-      count: bytes[4]
-    }
+      count: bytes[4],
+    },
   };
 }
